07e87ec4f2f0f683e2e6df32fb14f51570810e71
djacoronel <djacoronel@gmail.com>
2017-02-23 23:58:32 +0800

    Initial commit

---
+++ app/src/main/java/com/djacoronel/basiclauncher/GridAdapter.java
@@ -0,0 +1,81 @@
+package com.djacoronel.basiclauncher;
+
+import android.app.ActivityOptions;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class GridAdapter extends RecyclerView.Adapter<GridAdapter.ViewHolder> {
+    private PackageManager manager;
+    private List<MainActivity.AppDetail> apps;
+    private Context mContext;
+
+    GridAdapter(List<MainActivity.AppDetail> apps, Context context){
+        this.apps = apps;
+        mContext = context;
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+        TextView label;
+        ImageView icon;
+
+        ViewHolder(View itemView) {
+            super(itemView);
+            label = (TextView) itemView.findViewById(R.id.item_app_label);
+            icon = (ImageView) itemView.findViewById(R.id.item_app_icon);
+
+            itemView.setOnClickListener(
+                    new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            manager = mContext.getPackageManager();
+                            Intent i = manager.getLaunchIntentForPackage(apps.get(getAdapterPosition()).name.toString());
+
+                            Bundle optsBundle = null;
+                            ActivityOptions opts = null;
+                            if (Build.VERSION.SDK_INT >= 23) {
+                                int left = 0, top = 0;
+                                int width = v.getMeasuredWidth(), height = v.getMeasuredHeight();
+
+                                opts = ActivityOptions.makeClipRevealAnimation(v, left, top, width, height);
+                            } else if (!(Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP)) {
+                                // Below L, we use a scale up animation
+                                opts = ActivityOptions.makeScaleUpAnimation(v, 0, 0, v.getMeasuredWidth(), v.getMeasuredHeight());
+                            }
+                            optsBundle = opts != null ? opts.toBundle() : null;
+
+                            mContext.startActivity(i, optsBundle);
+                        }
+                    }
+            );
+        }
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.app_detail, parent, false);
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.icon.setImageDrawable(apps.get(position).icon);
+        holder.label.setText(apps.get(position).label);
+    }
+
+    @Override
+    public int getItemCount() {
+        return apps.size();
+    }
+}
---
+++ app/src/main/java/com/djacoronel/basiclauncher/MainActivity.java
@@ -0,0 +1,81 @@
+package com.djacoronel.basiclauncher;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.graphics.drawable.Drawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class MainActivity extends Activity {
+
+    private List<AppDetail> apps;
+    private GridView grid;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        loadApps();
+        loadAppGrid();
+    }
+
+    @Override
+    public void onBackPressed() {
+    }
+
+    public class AppDetail{
+        CharSequence label, name;
+        Drawable icon;
+    }
+
+    private void loadApps() {
+        PackageManager manager = getPackageManager();
+        apps = new ArrayList<>();
+
+        Intent i = new Intent(Intent.ACTION_MAIN, null);
+        i.addCategory(Intent.CATEGORY_LAUNCHER);
+
+        List<ResolveInfo> availableActivities = manager.queryIntentActivities(i, 0);
+
+        for(ResolveInfo ri : availableActivities) {
+            AppDetail app = new AppDetail();
+            app.label = ri.loadLabel(manager);
+            app.name = ri.activityInfo.packageName;
+            app.icon = ri.activityInfo.loadIcon(manager);
+            apps.add(app);
+        }
+
+        Collections.sort(apps, new Comparator<AppDetail>() {
+            @Override
+            public int compare(AppDetail a1, AppDetail a2) {
+                return a1.label.toString().compareToIgnoreCase(a2.label.toString());
+            }
+        });
+    }
+
+    public void loadAppGrid(){
+        RecyclerView grid = (RecyclerView) findViewById(R.id.app_grid);
+        GridAdapter adapter = new GridAdapter(apps, this);
+
+        grid.setLayoutManager(new GridLayoutManager(this,4));
+        grid.setAdapter(adapter);
+    }
+}
