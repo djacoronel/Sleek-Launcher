ec7d2c6c655b71fe61914a97f7c6d96d58ca8171
djacoronel <djacoronel@gmail.com>
2017-02-24 15:54:55 +0800

    Hidden navigation bar.

--- app/src/main/java/com/djacoronel/basiclauncher/MainActivity.java
+++ app/src/main/java/com/djacoronel/basiclauncher/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.graphics.drawable.Drawable;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
@@ -26,19 +27,31 @@ import java.util.List;
 public class MainActivity extends Activity {
 
     private List<AppDetail> apps;
-    private GridView grid;
+    View decorView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        decorView = getWindow().getDecorView();
         loadApps();
         loadAppGrid();
     }
 
     @Override
     public void onBackPressed() {
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            decorView.setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);}
     }
 
     public class AppDetail{
